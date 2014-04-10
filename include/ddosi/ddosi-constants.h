34be6828cca4b8e44528d3ff07f0259ca80b7522
Christopher Woodall <chris.j.woodall@gmail.com>
2014-04-10 17:42:26 -0400

    fixed missing paren

--- include/ddosi/ddosi-constants.h
+++ include/ddosi/ddosi-constants.h
@@ -13,15 +13,36 @@
 // Define Constants for DDS Controls, n represents channel.
 #define DDS_CS_OFFSET (3)
 #define DDS_CS_PIN(n) (DDS_CS_OFFSET + n)
+#define DDS_CS0_PIN   (3)
+#define DDS_CS1_PIN   (4)
+#define DDS_CS2_PIN   (5)
+#define DDS_CS3_PIN   (6)
+#define DDS_CS4_PIN   (7)
+#define DDS_CS5_PIN   (8)
+
 
 #define DDS_SDO_OFFSET (9)
 #define DDS_SDO_PIN(n) (DDS_SDO_OFFSET + n)
+#define DDS_SDO0_PIN   (9)
+#define DDS_SDO1_PIN   (10)
+#define DDS_SDO2_PIN   (11)
+#define DDS_SDO3_PIN   (12)
+#define DDS_SDO4_PIN   (13)
+#define DDS_SDO5_PIN   (14)
 
 #define DDS_SCLK_PIN 0
 #define DDS_IOUPDATE_PIN 1
 #define DDS_IORESET_PIN 2
 
-// Define LED constants (pin mappings
+// Define LED constants (pin mappings)
 #define LED_OFFSET 15
+#define LED_RGB_OFFSET 19
 #define LED_PIN(n) (LED_OFFSET+n)
-//#define ASM_NOP (asm("mov r0,r0"))
+
+#define LED_STATUS0_PIN 15
+#define LED_STATUS1_PIN 16
+#define LED_STATUS2_PIN 17
+#define LED_STATUS3_PIN 18
+#define LED_STATUSR_PIN 19
+#define LED_STATUSG_PIN 20
+#define LED_STATUSB_PIN 21
--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@
 #include "ddosi/bitbang-spi.h"
 #include "ddosi/ddosi-constants.h"
 
-#define DDS_FS 1E9
+#define DDS_FS (1E9)
 
 int main() {
 	int a;
@@ -25,7 +25,7 @@ int main() {
 	// Configure bit bang device
 	dds_bbspi_init(&dds_device, port);
 	dds_device.delay_interval_ts.tv_nsec = 50000L;
-	
+
 	// Enable channels
 	int enabled_channels = DDS_CH0;
 	// Options we may want to allow
@@ -35,34 +35,38 @@ int main() {
 	int cfr2_settings = (1<<24); // Enable Amplitude Scaling
 	
 	// Configures REFCLK_OUT (PLL config) FIXME
-	int cfr3_settings = (1<<28)    | // Low output current on refclk_out
-                   	  (0x01<<24) | // Setup VCO FIXME (FIXME)
-                   	  (0x01<<19  | // PLL Charge Pump Current (FIXME?)
-										  (1<<15)    | // Bypass refclk divider (FIXME?)
-		                  (1<<8);      // Enable PLL
-
+	int cfr3_settings = (1<<28) | // Low output current on refclk_out
+                   	  (1<<24) | // Setup VCO FIXME (FIXME)
+		                  (1<<19)  | // PLL Charge Pump Current (FIXME?)
+										  (1<<15) | // Bypass refclk divider (FIXME?)
+		                  (1<<8);   // Enable PLL                    
+											 
+	// Make a profile from different amplitude, phase and frequency settings
 	int profile0 = dds_form_profile(0x3fff, 0x0, frequency2ftw(200E6, DDS_FS));
 
+	// Setup CFR1
 	dds_device.ch_enable = enabled_channels;
 	dds_device.instruction = DDS_WRITE | DDS_CFR1;
 	dds_device.messages[0] = cfr1_settings;
 	dds_bbspi_write(&dds_device);
 
+	// Setup CFR2
 	dds_device.ch_enable = enabled_channels;
 	dds_device.instruction = DDS_WRITE | DDS_CFR2;
 	dds_device.messages[0] = cfr2_settings;
 	dds_bbspi_write(&dds_device);
 
+	// Setup CFR3
 	dds_device.ch_enable = enabled_channels;
 	dds_device.instruction = DDS_WRITE | DDS_CFR3;
 	dds_device.messages[0] = cfr3_settings;
 	dds_bbspi_write(&dds_device);
 
+	// Set profile0
 	dds_device.ch_enable = enabled_channels | DDS_EXTENDED_MESSAGE;
 	dds_device.instruction = DDS_WRITE | DDS_PROFILE_0;
 	dds_device.messages[0] = profile0;
 	dds_bbspi_write(&dds_device);
 
-	
 	return 0;
 }
